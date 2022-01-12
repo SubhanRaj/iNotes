076f6a8e48e77a7ace5954a787c67e1627f8aa42
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-12 23:37:58 +0530

    Basic note adding and displaying feature done

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?PHP
+// Connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databasename = "iNotes";
+// Create a connection
+
+$conn = mysqli_connect($servername, $username, $password, $databasename);
+
+// Die if connection was not successful
+if (!$conn) {
+    die("Sorry we failed to connect: " . mysqli_connect_error());
+}
+// 
+// Inserting data in database
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = $_POST["note_title"];
+    $desc = $_POST["note_desc"];
+    // SQL query to insert data in database
+    $sql = "INSERT INTO `notes` (`note_title`, `note_desc`) VALUES ('$title', '$desc')";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        // echo "The record has been inserted sucessfully<br>";
+        $insert = true;
+    } else {
+        echo "The record was not inserted sucessfull becaue of this error ---> " . mysqli_error($conn);
+    }
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -7,8 +39,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>iNotes : A PHP CRUD Notes App</title>
 </head>
@@ -18,9 +49,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">iNotes : A PHP CRUD Notes App</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -45,30 +74,57 @@
     <!-- FOrm for taking notes -->
     <div class="container my-3">
         <h2>Add a Note</h2>
-        <form>
+        <form action="/iNotes/index.php" method="POST">
             <div class="mb-3">
                 <label for="note_title" class="form-label">Note Title</label>
                 <input type="text" class="form-control" id="note_title" name="note_title" aria-describedby="textHelp">
 
             </div>
             <div class="mb-3">
-                <label for="note_description" class="form-label">Note Description</label>
-                <textarea class="form-control" id="note_description" name="note_description" rows="3"
-                    cols="10"></textarea>
+                <label for="note_desc" class="form-label">Note Description</label>
+                <textarea class="form-control" id="note_desc" name="note_desc" rows="3" cols="10"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Add Note</button>
         </form>
     </div>
     <div class="container">
-        <?PHP
-        
 
-        ?>
+
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">S. No.</th>
+                    <th scope="col">Note Title</th>
+                    <th scope="col">Note Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?PHP
+
+                $sql = "SELECT * FROM `notes`";
+                $result = mysqli_query($conn, $sql);
+                $row;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "
+                <tr>
+                    <th scope='row'>" . $row['sno'] . "</th>
+                    <td>" . $row['note_title'] . "</td>
+                    <td>" . $row['note_desc'] . "</td>
+                    <td>Actions</td>
+                </tr>
+                ";
+                }
+                ?>
+
+            </tbody>
+        </table>
+
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
 
 
